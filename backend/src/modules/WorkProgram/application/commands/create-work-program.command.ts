889a490e7f72008c92d52d154a1395f24fb5dd00
dhan <kyrawillowxd@gmail.com>
2026-07-11 14:31:00 +0700

    create application file command and service

---
+++ backend/src/modules/WorkProgram/application/commands/create-work-program.command.ts
@@ -0,0 +1,11 @@
+import { WorkProgramStatus } from "../../domain/work-program.entity";
+
+export class CreateWorkProgramCommand {
+    constructor(
+        public readonly name: string,
+        public readonly description: string,
+        public readonly status: WorkProgramStatus,
+        public readonly picId: string,
+        public readonly deadline: Date
+    ) {}
+}
---
+++ backend/src/modules/WorkProgram/application/commands/create-work-program.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@nestjs/common";
+import { WorkProgramRepository } from "../ports/work-program.repository.port";
+import { CreateWorkProgramCommand } from "./create-work-program.command";
+import { randomUUID } from "crypto";
+import { WorkProgramEntity } from "../../domain/work-program.entity";
+
+
+@Injectable()
+export class CreateWorkProgramService {
+    constructor(private workProgramRepo: WorkProgramRepository) {}
+
+    async execute(command: CreateWorkProgramCommand): Promise<string> {
+        
+        const id = randomUUID()
+        const workProgam = WorkProgramEntity.create(id, command.name, command.description, command.picId, command.deadline)
+        await this.workProgramRepo.save(workProgam)
+        return id
+    }
+}
